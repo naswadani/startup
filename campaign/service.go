4caf063b2f7d1702f51308df27c3cb54e9231aad
daninaswa <naswakhansa23@gmail.com>
2023-05-17 17:03:27 +0700

    handler campaign

--- campaign/service.go
+++ campaign/service.go
@@ -2,6 +2,7 @@ package campaign
 
 type Service interface {
 	GetCampaigns(userID int) ([]Campaign, error)
+	GetCampaignByID(input GetCampaignDetailInput) (Campaign, error)
 }
 
 type service struct {
@@ -21,6 +22,14 @@ func (s *service) GetCampaigns(userID int) ([]Campaign, error) {
 		return campaign, nil
 	}
 	campaign, err := s.repository.FindAll()
+	if err != nil {
+		return campaign, err
+	}
+	return campaign, nil
+}
+
+func (s *service) GetCampaignByID(input GetCampaignDetailInput) (Campaign,error){
+	campaign, err := s.repository.FindByID(input.ID)
 	if err != nil {
 		return campaign, err
 	}
--- handler/campaign.go
+++ handler/campaign.go
@@ -31,3 +31,23 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 	respose := helper.APIResponse("List of campaign", http.StatusOK, "succes", campaign.FormatCampaigns(campaigns))
 	c.JSON(http.StatusOK, respose)
 }
+
+func (h *campaignHandler) GetCampaign(c *gin.Context) {
+	var input campaign.GetCampaignDetailInput
+
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get detail campaign", http.StatusBadRequest, "error",nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	campaignDetail, err := h.service.GetCampaignByID(input)
+	if err != nil {
+		response := helper.APIResponse("Error to get detail campaign", http.StatusBadRequest, "error",nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	response := helper.APIResponse("Campaign detail",http.StatusOK, "success", campaignDetail)
+	c.JSON(http.StatusOK, response)
+}
